1d5d625130141b02789c1ce465793f36803137f1
Fadli Saad <fadlisaad@gmail.com>
2021-11-11 08:48:02 +0800

    Toggle comment
    Signed-off-by: Fadli Saad <fadlisaad@gmail.com>

--- app/helper.php
+++ app/helper.php
@@ -48,7 +48,7 @@ if (! function_exists('distance')) {
     }
   }
 
-  echo distance(32.9697, -96.80322, 29.46786, -98.53506, "M") . " Miles<br>";
-  echo distance(32.9697, -96.80322, 29.46786, -98.53506, "K") . " Kilometers<br>";
-  echo distance(32.9697, -96.80322, 29.46786, -98.53506, "N") . " Nautical Miles<br>";
+  // echo distance(32.9697, -96.80322, 29.46786, -98.53506, "M") . " Miles<br>";
+  // echo distance(32.9697, -96.80322, 29.46786, -98.53506, "K") . " Kilometers<br>";
+  // echo distance(32.9697, -96.80322, 29.46786, -98.53506, "N") . " Nautical Miles<br>";
 }
